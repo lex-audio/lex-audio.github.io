8e0c1217497739233095ca508b6855ea38d70224
Guilherme de Lemos <guilherme.eti@gmail.com>
2019-10-20 11:01:43 -0300

    index

---
+++ precache-manifest.e53d6c6be99e2fc9def0aa9e08fbcba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e40b47b62b03eb8d2aa6489214497cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e06a3fd2be15ad5690c2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7ca4430625888dec0ab6",
+    "url": "/static/js/2.3161608c.chunk.js"
+  },
+  {
+    "revision": "e06a3fd2be15ad5690c2",
+    "url": "/static/js/main.50b8fd17.chunk.js"
+  },
+  {
+    "revision": "a1c606264a46e6441035",
+    "url": "/static/js/runtime-main.3273d4c8.js"
+  }
+]);
